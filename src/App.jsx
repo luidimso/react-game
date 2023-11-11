d4f345801b7a17c916b7155950e03624cf30afa0
luidimso <luidi_mso@outlook.com>
2023-11-11 11:56:09 -0300

    Creating TimerChallenge component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="easy" targetTime="1"></TimerChallenge>
+        <TimerChallenge title="not easy" targetTime="5"></TimerChallenge>
+        <TimerChallenge title="hard" targetTime="10"></TimerChallenge>
+        <TimerChallenge title="harder" targetTime="15"></TimerChallenge>
+      </div>
     </>
   );
 }
---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,10 @@
+export default function TimerChallenge({title, targetTime}) {
+    return [
+        <section className="challenge">
+            <h2>{title}</h2>
+            <p className="challenge-time">{targetTime} second{targetTime > 1 ? "s" : ""}</p>
+            <button>Start Challenge</button>
+            <p className="active">Time running</p>
+        </section>
+    ];
+}
