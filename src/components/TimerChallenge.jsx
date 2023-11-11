db9af21a3d431ac462a6df8a823b106de6332fff
luidimso <luidi_mso@outlook.com>
2023-11-11 12:07:29 -0300

    Implementing start timer method

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function TimerChallenge({title, targetTime}) {
-    return [
+    const [isTimeExíred, setIsTimeExpired] = useState(false);
+    const [timerStarted, setTimerStarted] = useState(false);    
+    function startTimer() {
+        setTimeout(() => {
+            setIsTimeExpired(true);
+        }, targetTime * 1000);
+
+        setTimerStarted(true);
+    }
+
+
+    return (
         <section className="challenge">
             <h2>{title}</h2>
+            <p>{isTimeExíred ? "Time expired" : ""}</p>
             <p className="challenge-time">{targetTime} second{targetTime > 1 ? "s" : ""}</p>
-            <button>Start Challenge</button>
-            <p className="active">Time running</p>
+            <button onClick={startTimer}>{timerStarted ? "Stop" : "Start"} Challenge</button>
+            <p className="active">{timerStarted ? "Timer running" : "Timer stopped"}</p>
         </section>
-    ];
+    );
 }
