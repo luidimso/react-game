eed7bae2312b02d97f7f6e4ba3eb7e5a7e10887a
luidimso <luidi_mso@outlook.com>
2023-11-11 05:39:52 -0300

    Handle onChange event on input to take its value

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState("");
+  const [wasSubmitted, setWasSubmitted] = useState(false);
+
+  function handleChange(event) {
+    setWasSubmitted(false);
+    setPlayerName(event.target.value);
+  }
+
+  function hangleClick() {
+    setWasSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {wasSubmitted ? playerName : ""}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} />
+        <button onClick={hangleClick}>Set Name</button>
       </p>
     </section>
   );
